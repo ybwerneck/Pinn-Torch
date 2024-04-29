e2d6b01efb76f894f1fabc4e83ffd0e4dd4c9acf
yan bw <yanbwerneck@outlook.com>
2024-04-28 22:08:16 -0300

    add sechduler

--- Trainer.py
+++ Trainer.py
@@ -9,8 +9,8 @@ class Trainer:
         self.val_steps=val_steps
         self.print_steps=print_steps
         self.output_folder=output_folder
-        self.optimizer =  optim.Adam(model.parameters(), lr=1e-4)
-        self.scheduler = ReduceLROnPlateau(self.optimizer)
+        self.optimizer =  optim.Adam(model.parameters(), lr=1e-2)
+        self.scheduler = ReduceLROnPlateau(self.optimizer, mode='min', factor=0.5, patience=100, threshold=0.00001, threshold_mode='rel', cooldown=0, min_lr=0.001, eps=1e-08)
         try:
             os.mkdir(self.output_folder)
         except:
@@ -41,7 +41,7 @@ class Trainer:
             self.optimizer.step()
             self.scheduler.step(total_loss)
             if it % self.print_steps == 0:
-                print("Iteration ", it, ": total loss ", total_loss.item(),losses)
+              print("Iteration {}: total loss {:.4f}, losses: {}, learning rate: {:.10f}".format(it, total_loss.item(), losses, self.scheduler.get_last_lr()[0]))
                 
                 
             if it % self.val_steps ==0:
