f47756198413298d1ed67fb48a03588c265e93bb
user <yanbwerneck@outlook.com>
2024-05-05 00:42:34 -0300

    v1

---
+++ PINN_FRAMEWORK/dependencies.py
@@ -0,0 +1,15 @@
+# dependencies.py
+
+import torch
+import numpy as np
+import torch.nn.functional as F
+import random
+import tensorflow as tf
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import os
+import h5py
+import chaospy as cp
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
