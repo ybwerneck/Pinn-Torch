bf4d50b095f52803c62ae4ef77c0f4c249648a09
Esterci <thiago.esterci.fernandes@gmail.com>
2025-05-06 19:03:41 -0300

    refact(loss): added rmse loss and rewrite the loss init

--- fisiocomPinn/Loss.py
+++ fisiocomPinn/Loss.py
@@ -60,9 +60,6 @@ class LOSS(torch.nn.Module):
         batch, tgt = self.getBatch()
         pred = model(batch)
 
-        if self.i > self.len_d:
-            self.i = 0
-
         return self.loss(tgt, pred)
 
     def loss(self, tgt, pred):
@@ -70,32 +67,90 @@ class LOSS(torch.nn.Module):
 
 
 class MAE(LOSS):
-    def __init__(self, data_in, target, batch_size=10000, shuffle=False):
-        super(MAE, self).__init__(data_in, target, batch_size, shuffle)
+    def __init__(
+        self,
+        data_in,
+        target,
+        device="cpu",
+        name="loss",
+        batch_size=10000,
+        shuffle=False,
+    ):
+        super(MAE, self).__init__(
+            data_in, target, batch_size, shuffle, device=device, name=name
+        )
 
     def loss(self, tgt, pred):
         return torch.mean(torch.abs(tgt - pred))
 
 
 class MSE(LOSS):
-    def __init__(self, data_in, target, batch_size=10000, shuffle=False):
-        super(MSE, self).__init__(data_in, target, batch_size, shuffle)
+    def __init__(
+        self,
+        data_in,
+        target,
+        device="cpu",
+        name="loss",
+        batch_size=10000,
+        shuffle=False,
+    ):
+        super(MSE, self).__init__(
+            data_in, target, batch_size, shuffle, device=device, name=name
+        )
 
     def loss(self, tgt, pred):
-        return torch.mean((tgt - pred) ** 2)
+        return torch.mean((tgt - pred).ravel() ** 2)
+
+
+class RMSE(LOSS):
+    def __init__(
+        self,
+        data_in,
+        target,
+        device="cpu",
+        name="loss",
+        batch_size=10000,
+        shuffle=False,
+    ):
+        super(RMSE, self).__init__(
+            data_in, target, batch_size, shuffle, device=device, name=name
+        )
+
+    def loss(self, tgt, pred):
+        return torch.mean(torch.sum(((tgt - pred)) ** 2, dim=1) ** 0.5)
 
 
 class KLDivergenceLoss(LOSS):
-    def __init__(self, data_in, target, batch_size=10000, shuffle=False):
-        super(KLDivergenceLoss, self).__init__(data_in, target, batch_size, shuffle)
+    def __init__(
+        self,
+        data_in,
+        target,
+        device="cpu",
+        name="loss",
+        batch_size=10000,
+        shuffle=False,
+    ):
+        super(KLDivergenceLoss, self).__init__(
+            data_in, target, batch_size, shuffle, device=device, name=name
+        )
 
     def loss(self, tgt, pred):
         return F.kl_div(pred, tgt)
 
 
 class CosineSimilarityLoss(LOSS):
-    def __init__(self, data_in, target, batch_size=10000, shuffle=False):
-        super(CosineSimilarityLoss, self).__init__(data_in, target, batch_size, shuffle)
+    def __init__(
+        self,
+        data_in,
+        target,
+        device="cpu",
+        name="loss",
+        batch_size=10000,
+        shuffle=False,
+    ):
+        super(CosineSimilarityLoss, self).__init__(
+            data_in, target, batch_size, shuffle, device=device, name=name
+        )
 
     def loss(self, tgt, pred):
         return 1 - F.cosine_similarity(pred, tgt)
