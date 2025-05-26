b5ab29d525036d469c5c5010853dd2f538ecb7a4
Esterci <thiago.esterci.fernandes@gmail.com>
2025-05-26 18:20:34 -0300

    feat(Losses):  new pinn loss and ways to use the defined error metrics

--- fisiocomPinn/Loss_PINN.py
+++ fisiocomPinn/Loss_PINN.py
@@ -1,36 +1,86 @@
 from .dependencies import *
+from .Loss import *
+
+loss_map = {
+    "MAE": MAE,
+    "MSE": MSE,
+    "RMSE": RMSE,
+    "KLDivergenceLoss": KLDivergenceLoss,
+    "CosineSimilarityLoss": CosineSimilarityLoss,
+    "LPthLoss": LPthLoss,
+}
 
 
-# Loss base class
 class LOSS_PINN(torch.nn.Module):
 
     def __init__(
         self,
-        Pinn_func,
-        batch_generator,
         batch_size=10000,
-        shuffle=False,
         device=torch.device("cuda"),
+        loss="RMSE",
         name="LossPinn",
     ):
-        self.f = Pinn_func
-        self.batch_generator = batch_generator
-        self.name = name
         self.batch_size = batch_size
-        self.shuffle = shuffle
-
         self.device = device
+        self.loss = loss
+        self.name = name
         super(LOSS_PINN, self).__init__()
 
-    def getBatch(self):
-        batch = self.batch_generator(self.batch_size, self.device)
+    def setBatchGenerator(self, batch_generator, *batch_args):
+        self.batch_generator = batch_generator
+        self.batch_args = batch_args
 
-        return batch
+    def setPinnFunction(self, pinn_func, *pinn_args):
+        self.pinn_func = pinn_func
+        self.pinn_args = pinn_args
 
-    def forward(self, model):
-        batch = self.getBatch()
+    def forward(
+        self,
+        model,
+    ):
+        batch, tgt = self.batch_generator(
+            self.batch_size, self.device, *self.batch_args
+        )
 
-        return self.f(batch, model)
+        criterium = loss_map[self.loss](batch, tgt)
 
-    def loss(self, tgt, pred):
-        print("Not defined")
+        pred = model(batch)
+
+        pinn_pred = self.pinn_func(pred, batch, *self.pinn_args)
+
+        return criterium.loss(tgt, pinn_pred)
+
+
+class LOSS_INITIAL(torch.nn.Module):
+
+    def __init__(
+        self,
+        batch_size=10000,
+        device=torch.device("cuda"),
+        loss="RMSE",
+        name="LossInital",
+    ):
+        self.batch_size = batch_size
+        self.device = device
+        self.loss = loss
+        self.name = name
+        super(LOSS_INITIAL, self).__init__()
+
+    def setBatchGenerator(self, batch_generator, *args):
+        self.batch_generator = batch_generator
+        self.batch_args = args
+
+    def forward(
+        self,
+        model,
+    ):
+
+        batch, tgt = self.batch_generator(
+            self.batch_size, self.device, *self.batch_args
+        )
+
+        criterium = loss_map[self.loss](batch, tgt)
+
+        pred = model(batch)
+
+        return criterium.loss(tgt, pred)
